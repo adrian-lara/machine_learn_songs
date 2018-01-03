7e1b1c2968a7fcfedae231336bff9e9ad1b5c13a
Adrian Lara <adrianblara@gmail.com>
2018-01-03 16:46:29 -0700

    Write guest visits homepage test

---
+++ spec/features/guest/guest_can_visit_homepage_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "As a guest, when I visit '/'" do
+  it "then I see 'Log In', 'How It Works', and 'SiteName' links" do
+    visit '/'
+
+    expect(page).to have_link("Log In")
+    within(".footer") do
+      expect(page).to have_link("How It Works")
+    end
+    within(".nav-bar") do
+      expect(page).to have_link("SiteName")
+    end
+  end
+end
