5c4022d885a1d3bd50037e0ca402bc91032e661a
Adrian Lara <adrianblara@gmail.com>
2018-01-04 18:14:26 -0700

    Spotify Auth Unrefactored: Route for spotify/callback and user dashboard opened

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   get '/', to: "welcome#index", as: :root
 
+  get "/spotify/callback", to: "session#create"
+
+  get "/dashboard", to: "users/dashboard#index", as: :dashboard
 end
