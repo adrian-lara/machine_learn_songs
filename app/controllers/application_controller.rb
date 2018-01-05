f3a0550e47d971fba72c5d2b28ded8a0076304e7
Adrian Lara <adrianblara@gmail.com>
2018-01-04 18:17:04 -0700

    Spotify Auth Unrefactored: Application Controller current_user and refresh_access_token methods written

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def refresh_access_token
+    conn = Faraday.new(url: "https://accounts.spotify.com")
+    response = conn.post do |req|
+      req.url "/api/token"
+      req.body = "client_id=#{ENV['client_id']}&client_secret=#{ENV['client_secret']}&grant_type=refresh_token&refresh_token=#{current_user.refresh_token}"
+    end
+    raw_user_tokens = JSON.parse(response.body, symbolize_names: true)
+
+    current_user.update(access_token: raw_user_tokens[:access_token])
+  end
 end
