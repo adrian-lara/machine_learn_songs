5b06abf6601796efa9bb354fccae4d3883625ae8
Adrian Lara <adrianblara@gmail.com>
2018-01-04 22:49:24 -0700

    Add and set up vcr gem

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ group :development, :test do
   gem 'pry'
   gem 'capybara'
   gem 'launchy'
+  gem 'vcr'
+  gem 'webmock'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,16 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'vcr'
 
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.filter_sensitive_data('<CLIENT_ID>') { ENV["client_id"] }
+  c.filter_sensitive_data('<CLIENT_SECRET>') { ENV["client_secret"] }
+  c.filter_sensitive_data('<access_token>') { ENV["access_token"] }
+  c.filter_sensitive_data('<refresh_token>') { ENV["refresh_token"] }
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
