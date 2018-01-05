35871a950b2c1607e15ab05b4e005885f1fd5015
Adrian Lara <adrianblara@gmail.com>
2018-01-04 18:15:46 -0700

    Spotify Auth Unrefactored: Session Controller create action (callback location) contains spotify auth handshake process

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,33 @@
+class SessionController < ApplicationController
+
+  def create
+    if params[:code]
+      conn = Faraday.new(url: "https://accounts.spotify.com")
+      response = conn.post do |req|
+        req.url "/api/token"
+        req.body = "client_id=#{ENV['client_id']}&client_secret=#{ENV['client_secret']}&grant_type=authorization_code&code=#{params[:code]}&redirect_uri=http://localhost:3000/spotify/callback"
+      end
+      raw_user_tokens = JSON.parse(response.body, symbolize_names: true)
+
+      user_conn = Faraday.new(url: "https://api.spotify.com")
+      user_data_response = user_conn.get do |req|
+        req.url "/v1/me"
+        req.headers['Authorization'] = "Bearer #{raw_user_tokens[:access_token]}"
+      end
+      raw_user_data = JSON.parse(user_data_response.body, symbolize_names: true)
+
+      user = User.find_or_create_by(uid: raw_user_data[:id])
+      user.update(
+        access_token: raw_user_tokens[:access_token],
+        refresh_token: raw_user_tokens[:refresh_token],
+        token_type: raw_user_tokens[:token_type]
+      )
+
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      redirect_to root_path
+    end
+  end
+
+end
