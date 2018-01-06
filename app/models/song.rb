cdcb823e46fd360f92366c9e45d50ccad70fc8ed
Adrian Lara <adrianblara@gmail.com>
2018-01-06 14:16:22 -0700

    Song Object created

---
+++ app/models/song.rb
@@ -0,0 +1,26 @@
+class Song
+
+  attr_reader :title, :artists, :album, :album_cover
+
+  def initialize(song_info)
+    @title = song_info[:item][:name]
+    @artists = get_artists(song_info)
+    @album = song_info[:item][:album][:name]
+    @album_cover = get_album_cover(song_info)
+  end
+
+  private
+
+    def get_artists(song_info)
+      song_info[:item][:artists].map do |artist_info|
+        artist_info[:name]
+      end.join(", ")
+    end
+
+    def get_album_cover(song_info)
+      images = song_info[:item][:album][:images]
+      second_smallest = images[images.count - 2]
+      second_smallest[:url] if second_smallest
+    end
+
+end
