9475b9b14f350b7bf0eeaa86d10c7d8e5140621d
Adrian Lara <adrianblara@gmail.com>
2018-01-06 14:16:56 -0700

    Song Factory Created

---
+++ spec/factories/song.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :song do
+    sequence(:title) { |n| "title_#{n}" }
+    sequence(:artists) { |n| "artist_#{n}" }
+    sequence(:album) { |n| "album_#{n}" }
+    album_cover "https://i.scdn.co/image/70429aaeceb7f8f6c087133382728223e0004b29"
+  end
+end
