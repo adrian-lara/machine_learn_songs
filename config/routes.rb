df99247f48846d53f120066614d8f03860f7df48
Adrian Lara <adrianblara@gmail.com>
2018-01-07 01:47:25 -0700

    Route opened for logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   get "/spotify/callback", to: "session#create"
 
+  delete '/logout', to: "session#destroy", as: :logout
+
   get "/dashboard", to: "users/dashboard#index", as: :dashboard
+
 end
