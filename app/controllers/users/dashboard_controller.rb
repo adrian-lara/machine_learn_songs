b82f07018305fcb955dbbf7f90e2767e3d40a12c
Adrian Lara <adrianblara@gmail.com>
2018-01-07 23:48:19 -0700

    User Dashboard Controller now uses spotify service for API call

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -2,16 +2,8 @@ class Users::DashboardController < ApplicationController
   before_action :check_user
 
   def index
-    conn = Faraday.new("https://api.spotify.com")
-    response = conn.get do |req|
-      req.url "/v1/me/player/currently-playing"
-      req.headers['Authorization'] = "Bearer #{current_user.access_token}"
-    end
-
-    unless response.body.empty?
-      raw_song_data = JSON.parse(response.body, symbolize_names: true)
-      @song = Song.new(raw_song_data, current_user)
-    end
+    service = SpotifyService.new(current_user)
+    @song = service.currently_playing
   end
 
 end
