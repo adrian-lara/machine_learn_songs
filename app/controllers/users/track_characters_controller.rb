b55b5bdc5abd0d3c8eda9f00f755f526658ebd3c
Adrian Lara <adrianblara@gmail.com>
2018-01-07 23:49:01 -0700

    User Track Character Controller now uses uses SongAssessmentService to call SpotifyService for API call

--- app/controllers/users/track_characters_controller.rb
+++ app/controllers/users/track_characters_controller.rb
@@ -1,13 +1,7 @@
 class Users::TrackCharactersController < ApplicationController
 
   def create
-    conn = Faraday.new(url: "https://api.spotify.com/")
-    response = conn.get do |req|
-      req.url "v1/audio-features/#{params[:track_id]}"
-      req.headers['Authorization'] = "#{current_user.token_type} #{current_user.access_token}"
-    end
-    track_character_data = JSON.parse(response.body, symbolize_names: true)
-    SongAssessmentService.assess(track_character_data, current_user, params[:assessment])
+    SongAssessmentService.assess(current_user, params[:track_id], params[:assessment])
 
     redirect_to dashboard_path
   end
