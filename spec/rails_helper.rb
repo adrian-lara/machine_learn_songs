8118f0814c93b8e08f5f645535d0fccf19946ed0
Adrian Lara <adrianblara@gmail.com>
2018-01-08 22:23:24 -0700

    Change Database cleaner default strategy to :truncation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,8 +41,8 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
   end
 
   config.include FactoryBot::Syntax::Methods
