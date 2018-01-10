d4ebd272fa4610e0d9f2d403fa850c8268ec06f3
Adrian Lara <adrianblara@gmail.com>
2018-01-09 17:34:58 -0700

    Prediction PORO (that executes Bayesian algorithm) written and tested

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -2,16 +2,25 @@ class Prediction
 
   def initialize(user)
     @user = user
-    @songs = user.track_characters
   end
 
-  def result
-    binding.pry
+  def result(input_song)
+    prb_like = prb_like_given_song(input_song)
+    prb_dislike = prb_dislike_given_song(input_song)
+    PredictionResult.new(prb_like, prb_dislike)
   end
 
   private
 
-    attr_reader :user, :songs
+    attr_reader :user
+
+    def bayes_attributes
+      @bayes_attributes ||= TrackCharacter.calculable_attributes
+    end
+
+    def users_songs
+      @users_songs ||= user.track_characters
+    end
 
     def liked_songs
       @liked_songs ||= user.liked_songs
@@ -21,46 +30,82 @@ class Prediction
       @disliked_songs ||= user.disliked_songs
     end
 
-    def probability_like
-      @probability_like ||= (liked_songs.count.to_f / songs.count)
+    def like_frequency
+      @like_frequency ||= (liked_songs.count.to_f / users_songs.count)
     end
 
-    def probability_dislike
-      @probability_dislike ||= (disliked_songs.count.to_f / songs.count)
+    def dislike_frequency
+      @dislike_frequency ||= (disliked_songs.count.to_f / users_songs.count)
     end
 
-    def liked_probability_params
-      @liked_probability_params ||= TrackCharacter.calculable_attributes
+    def liked_pdf_params
+      @liked_pdf_params ||= pdf_params(liked_songs)
+    end
+
+    def disliked_pdf_params
+      @disliked_pdf_params ||= pdf_params(disliked_songs)
+    end
+
+    def pdf_params(songs)
+      bayes_attributes
         .reduce({}) do |params, attrib|
-          params[attrib.to_sym] = attr_params(attrib, liked_songs)
-          params
-        end
-    end
-
-    def disliked_probability_params
-      @disliked_probability_params ||= TrackCharacter.calculable_attributes
-        .reduce({}) do |params, attrib|
-          params[attrib.to_sym] = attr_params(attrib, disliked_songs)
+          params[attrib.to_sym] = attr_params(attrib, songs)
           params
         end
     end
 
     def attr_params(attrib, songs)
       {
-        mean: calculate_mean(attrib, songs),
-        variance: calculate_variance(attrib, songs)
+        mean: mean(attrib, songs),
+        variance: variance(attrib, songs)
       }
     end
 
-    def calculate_mean(attrib, songs)
+    def mean(attrib, songs)
       songs.average(attrib.to_sym)
     end
 
-    def calculate_variance(attrib, songs)
+    def variance(attrib, songs)
       attrib_values = songs.pluck(attrib.to_sym)
-      mean = calculate_mean(attrib, songs)
+      mean = mean(attrib, songs)
 
       sq_differences = attrib_values.map { |v| (v - mean) ** 2 }
       sq_differences.reduce(:+) / sq_differences.count
+    end
+
+    def normal_pdf(x, attr_params)
+      two_pi_variance = 2.0 * Math::PI * attr_params[:variance]
+      sq_difference = (x - attr_params[:mean]) ** 2
+      e_exponent = -0.5 * (sq_difference / attr_params[:variance])
+
+      (1.0 / Math.sqrt(two_pi_variance)) * (Math::E ** e_exponent)
+    end
+
+    def prb_song_given_like(song)
+      bayes_attributes.reduce(1) do |total, attrib|
+        total *= normal_pdf(song.send(attrib), liked_pdf_params[attrib.to_sym])
+        total
+      end
+    end
+
+    def prb_song_given_dislike(song)
+      bayes_attributes.reduce(1) do |total, attrib|
+        total *= normal_pdf(song.send(attrib), disliked_pdf_params[attrib.to_sym])
+        total
+      end
+    end
+
+    def prb_like_given_song(song)
+      (prb_song_given_like(song) * like_frequency) / (
+        (prb_song_given_like(song) * like_frequency) +
+        (prb_song_given_dislike(song) * dislike_frequency)
+      )
+    end
+
+    def prb_dislike_given_song(song)
+      (prb_song_given_dislike(song) * dislike_frequency) / (
+        (prb_song_given_like(song) * like_frequency) +
+        (prb_song_given_dislike(song) * dislike_frequency)
+      )
     end
 end
--- spec/models/prediction_spec.rb
+++ spec/models/prediction_spec.rb
@@ -13,9 +13,49 @@ describe Prediction do
   end
 
   describe "Instance Methods" do
-    describe "#" do
-      it "returns" do
-binding.pry
+    describe "#result" do
+      it "returns PredictionResult for a very likable song with like_score of 100.0 and a dislike_score of 0.0" do
+        very_likable_song = TrackCharacter.new(
+          acousticness:         0.74,
+          danceability:         0.74,
+          duration_ms:          74,
+          energy:               0.74,
+          instrumentalness:     0.74,
+          key:                  74,
+          liveness:             0.74,
+          loudness:             0.74,
+          mode:                 74,
+          speechiness:          0.74,
+          tempo:                0.74,
+          time_signature:       74,
+          valence:              0.74
+        )
+        result = @prediction.result(very_likable_song)
+        expect(result).to be_a PredictionResult
+        expect(result.like_score).to eq(100.0)
+        expect(result.dislike_score).to eq(0.0)
+      end
+
+      it "returns PredictionResult for a very dislikable song with like_score of 0.0 and a dislike_score of 100.0" do
+        very_dislikable_song = TrackCharacter.new(
+          acousticness:         0.24,
+          danceability:         0.24,
+          duration_ms:          24,
+          energy:               0.24,
+          instrumentalness:     0.24,
+          key:                  24,
+          liveness:             0.24,
+          loudness:             0.24,
+          mode:                 24,
+          speechiness:          0.24,
+          tempo:                0.24,
+          time_signature:       24,
+          valence:              0.24
+        )
+        result = @prediction.result(very_dislikable_song)
+        expect(result).to be_a PredictionResult
+        expect(result.like_score).to eq(0.0)
+        expect(result.dislike_score).to eq(100.0)
       end
     end
   end
