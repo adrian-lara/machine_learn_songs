f3bc21571142f4eaf0ff76b9508eb192673adab2
Adrian Lara <adrianblara@gmail.com>
2018-01-10 14:17:28 -0700

    User::PredictController's index action now generates PredictionResult instance via Prediction class

--- app/controllers/users/predict_controller.rb
+++ app/controllers/users/predict_controller.rb
@@ -1,8 +1,13 @@
 class Users::PredictController < ApplicationController
 
   def index
-    service = SpotifyService.new(current_user)
-    @song = service.currently_playing
+    api_service = SpotifyService.new(current_user)
+    @song = api_service.currently_playing
+
+    audio_features = api_service.get_audio_features(@song.track_id)
+    track_character = SongAssessmentService.find_or_create_track_character(audio_features)
+    prediction = Prediction.new(current_user)
+    @result = prediction.result(track_character)
   end
 
 end
