fb097ee9d56a9b5bc4ec5c72cf1bad5b9a7355de
Adrian Lara <adrianblara@gmail.com>
2018-01-10 18:51:44 -0700

    Move a few pieces of text and remove "How it Works" link.

--- spec/features/guest/guest_can_visit_homepage_spec.rb
+++ spec/features/guest/guest_can_visit_homepage_spec.rb
@@ -5,9 +5,6 @@ describe "As a guest, when I visit '/'" do
     visit '/'
 
     expect(page).to have_link("Log In")
-    within(".footer") do
-      expect(page).to have_link("How It Works")
-    end
     within(".nav-bar") do
       expect(page).to have_link("MachineLearnSongs")
     end
